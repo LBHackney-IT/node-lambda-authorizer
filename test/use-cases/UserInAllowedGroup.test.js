c931c61a3f95aae424e5d5228a88d0919cdf1389
Matt Millar <matthew.millar@madetech.com>
2020-04-28 14:43:43 +0100

    Minor refactoring for readability

--- test/use-cases/UserInAllowedGroup.test.js
+++ test/use-cases/UserInAllowedGroup.test.js
@@ -7,6 +7,12 @@ describe("userInAllowedGroup", function() {
     expect(result).toBe(false);
   });
 
+  it("returns false if the groups are empty", async function() {
+    const result = userInAllowedGroup([], []);
+
+    expect(result).toBe(false);
+  });
+
   it("retuns true if user is in the allowed group", async function() {
     const result = userInAllowedGroup(["allowed"], ["allowed", "group"]);
 
