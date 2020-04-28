9d5aaf29385ed4416aab4f614d37b61fba9fc54a
csabagyorfi <csaba@madetech.com>
2020-04-28 11:17:29 +0100

    Refactor authorizer into seperate usecases and add tests

---
+++ test/use-cases/Authorizer.test.js
@@ -0,0 +1,44 @@
+const Authorizer = require("../../lib/use-cases/Authorizer");
+const jwt = require("jsonwebtoken");
+
+describe("Authorizer", function() {
+  it('retuns "Unauthorized" if the request does not have the right secrets', async function() {
+    const jwtSecret = "secret";
+    const allowedGroups = ["Friends"];
+    const authorizer = new Authorizer({
+      jwtSecret: jwtSecret,
+      allowedGroups: allowedGroups
+    });
+    var result = await authorizer.execute({});
+
+    expect(result).toBe("Unauthorized");
+  });
+
+  it("allows a request that have the right secrets", async function() {
+    const jwtSecret = "secret";
+    const allowedGroups = ["Friends"];
+    const authorizer = new Authorizer({
+      jwtSecret: jwtSecret,
+      allowedGroups: allowedGroups
+    });
+
+    const token = jwt.sign(
+      { token: "super-secret-token", groups: ["Friends"] },
+      jwtSecret
+    );
+
+    var result = await authorizer.execute({
+      type: "TOKEN",
+      headers: { Authorization: `Bearer ${token}` },
+      methodArn: "arn:aws:execute-api:{dummy}"
+    });
+
+    expect(result.policyDocument.Statement).toEqual([
+      {
+        Action: "execute-api:Invoke",
+        Effect: "Allow",
+        Resource: "arn:aws:execute-api:{dummy}"
+      }
+    ]);
+  });
+});
---
+++ test/use-cases/DecodeToken.test.js
@@ -0,0 +1,18 @@
+const decodeToken = require("../../lib/use-cases/DecodeToken");
+const jwt = require("jsonwebtoken");
+
+describe("decodeToken", function() {
+  it("retuns false if the token does not have the jwt secret", async function() {
+    const result = decodeToken("decoded_token", "secret");
+
+    expect(result).toBe(false);
+  });
+
+  it("retuns the decoded token if the encrypted token has the right signiture", async function() {
+    const secret = "some secret";
+    const encryptedToken = jwt.sign("super-secret-token", secret);
+    const result = decodeToken(encryptedToken, secret);
+
+    expect(result).toBe("super-secret-token");
+  });
+});
