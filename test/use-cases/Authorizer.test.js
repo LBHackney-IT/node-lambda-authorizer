d49fdc5ce2c6fd25c97d4bc9ecd4a7bfdebc2a48
Matt Millar <matthew.millar@madetech.com>
2020-04-28 14:46:09 +0100

    Updated prettier/eslint config and prettify

--- test/use-cases/Authorizer.test.js
+++ test/use-cases/Authorizer.test.js
@@ -1,43 +1,43 @@
-const Authorizer = require("../../lib/use-cases/Authorizer");
-const jwt = require("jsonwebtoken");
+const Authorizer = require('../../lib/use-cases/Authorizer');
+const jwt = require('jsonwebtoken');
 
-describe("Authorizer", function() {
+describe('Authorizer', function() {
   it('retuns "Unauthorized" if the request does not have the right secrets', async function() {
-    const jwtSecret = "secret";
-    const allowedGroups = ["Friends"];
+    const jwtSecret = 'secret';
+    const allowedGroups = ['Friends'];
     const authorizer = new Authorizer({
       jwtSecret: jwtSecret,
       allowedGroups: allowedGroups
     });
     var result = await authorizer.execute({});
 
-    expect(result).toBe("Unauthorized");
+    expect(result).toBe('Unauthorized');
   });
 
-  it("allows a request that have the right secrets", async function() {
-    const jwtSecret = "secret";
-    const allowedGroups = ["Friends"];
+  it('allows a request that have the right secrets', async function() {
+    const jwtSecret = 'secret';
+    const allowedGroups = ['Friends'];
     const authorizer = new Authorizer({
       jwtSecret: jwtSecret,
       allowedGroups: allowedGroups
     });
 
     const token = jwt.sign(
-      { token: "super-secret-token", groups: ["Friends"] },
+      { token: 'super-secret-token', groups: ['Friends'] },
       jwtSecret
     );
 
     var result = await authorizer.execute({
-      type: "TOKEN",
+      type: 'TOKEN',
       headers: { Authorization: `Bearer ${token}` },
-      methodArn: "arn:aws:execute-api:{dummy}"
+      methodArn: 'arn:aws:execute-api:{dummy}'
     });
 
     expect(result.policyDocument.Statement).toEqual([
       {
-        Action: "execute-api:Invoke",
-        Effect: "Allow",
-        Resource: "arn:aws:execute-api:{dummy}"
+        Action: 'execute-api:Invoke',
+        Effect: 'Allow',
+        Resource: 'arn:aws:execute-api:{dummy}'
       }
     ]);
   });
--- test/use-cases/ExtractTokenFromCookieHeader.test.js
+++ test/use-cases/ExtractTokenFromCookieHeader.test.js
@@ -1,17 +1,17 @@
-const extractTokenFromCookieHeader = require("../../lib/use-cases/ExtractTokenFromCookieHeader");
+const extractTokenFromCookieHeader = require('../../lib/use-cases/ExtractTokenFromCookieHeader');
 
-describe("ExtractTokenFromCookieHeader", function() {
-  it("retuns null if headers.Authorization not found", async function() {
+describe('ExtractTokenFromCookieHeader', function() {
+  it('retuns null if headers.Authorization not found', async function() {
     const result = extractTokenFromCookieHeader({});
 
     expect(result).toBe(null);
   });
 
-  it("retuns the token when hackney token exists in the cookies", async function() {
+  it('retuns the token when hackney token exists in the cookies', async function() {
     const result = extractTokenFromCookieHeader({
-      headers: { Cookie: "hackneyToken=THISISATOKEN1234" }
+      headers: { Cookie: 'hackneyToken=THISISATOKEN1234' }
     });
 
-    expect(result).toBe("THISISATOKEN1234");
+    expect(result).toBe('THISISATOKEN1234');
   });
 });
